9b8c3cf4d727ddbea7c42247b017a3ca32b71962
lutz2000 <lutaayayusuf8@gmail.com>
2026-07-15 13:35:38 +0300

    fix: merge fmt fix into existing post_install block

--- plugins/withFmtFix.js
+++ plugins/withFmtFix.js
@@ -8,26 +8,24 @@ module.exports = function withFmtFix(config) {
     async (config) => {
       const podfilePath = path.join(config.modRequest.platformProjectRoot, 'Podfile');
       let podfile = fs.readFileSync(podfilePath, 'utf8');
-
+      if (podfile.includes('FMT_USE_NONTYPE_TEMPLATE_ARGS')) return config;
       const patch = `
-post_install do |installer|
-  installer.pods_project.targets.each do |target|
-    target.build_configurations.each do |config|
-      config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] ||= ['$(inherited)']
-      unless config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'].include?('FMT_USE_NONTYPE_TEMPLATE_ARGS=0')
-        config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] << 'FMT_USE_NONTYPE_TEMPLATE_ARGS=0'
+    installer.pods_project.targets.each do |target|
+      target.build_configurations.each do |config|
+        config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] ||= ['$(inherited)']
+        unless config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'].include?('FMT_USE_NONTYPE_TEMPLATE_ARGS=0')
+          config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] << 'FMT_USE_NONTYPE_TEMPLATE_ARGS=0'
+        end
+        config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '15.1'
       end
-      config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '15.1'
     end
-  end
-end
 `;
-
-      if (!podfile.includes('FMT_USE_NONTYPE_TEMPLATE_ARGS')) {
-        podfile += patch;
-        fs.writeFileSync(podfilePath, podfile);
+      if (podfile.includes('post_install do |installer|')) {
+        podfile = podfile.replace('post_install do |installer|', 'post_install do |installer|\n' + patch);
+      } else {
+        podfile += '\npost_install do |installer|\n' + patch + '\nend\n';
       }
-
+      fs.writeFileSync(podfilePath, podfile);
       return config;
     },
   ]);
